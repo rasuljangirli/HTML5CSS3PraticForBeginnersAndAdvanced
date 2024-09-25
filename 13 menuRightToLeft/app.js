0dabae1c0852470cfdda6b325d9aebac98d3b7f8
rasuljangirli <resulcahangirli99@gmail.com>
2024-09-25 12:20:26 +0400

    new folder

---
+++ 13 menuRightToLeft/app.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector('.sidebar');
+
+function showSidebar() {
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    sidebar.style.display = 'none';
+}
